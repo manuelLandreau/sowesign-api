60c39c98105672fd697b2169922636d97bfb286f
mlandreau <manuel.landreau@netapsys.fr>
2018-07-05 14:20:55 +0200

    pret pour prod

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -2,11 +2,13 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 // require('dotenv').config();
 
+const port = process.env.PORT || 3000;
+
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.port);
+    await app.listen(port);
 
     if (module.hot) {
         module.hot.accept();
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 // require('dotenv').config()
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.port);
+    await app.listen(port);
 }
 bootstrap();
