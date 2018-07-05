ffb4cb50e1877364b0d8f9346d6be76c9c609b97
mlandreau <manuel.landreau@netapsys.fr>
2018-07-05 14:19:21 +0200

    pret pour prod

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -6,7 +6,7 @@ declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(process.env.port);
 
     if (module.hot) {
         module.hot.accept();
